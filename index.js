44e7d73fe5ec304763f9201f803132a316f28983
timmson <timmson666@mail.ru>
2021-10-11 21:27:19 +0300

    Add link to chat at footer to every question #5

---
+++ index.js
@@ -0,0 +1,13 @@
+const fs = require("./lib/fs");
+const QuestionMapper = require("./lib/question-mapper");
+
+const questionMapper = new QuestionMapper("\r\n");
+
+
+const questions = fs.getListOfMDFiles(__dirname + "/questions/")
+    .map((fileName) => fs.readFile(__dirname + "/questions/" + fileName))
+    .map((fileContents) => questionMapper.map(fileContents));
+
+fs.writeFileContent(__dirname + "/lib/questions.js",  "module.exports =" + JSON.stringify(questions) + ";").then(() => console.log("Done"));
+
+//console.log(JSON.stringify(questionMapper.map(fs.readFile("./questions/0001-TDD.md")), null, 2));
