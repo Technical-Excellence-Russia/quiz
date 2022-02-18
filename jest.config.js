cd11cf247155316d9b4a0e469f2e9e018e892de7
timmson <timmson666@mail.ru>
2022-02-18 17:27:29 +0300

    Migrate to React #10

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+    collectCoverage: true,
+    collectCoverageFrom: ["./src/*", "./lib/*"],
     coverageReporters: ["lcov"],
     testMatch: ["**/test/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"]
 };
